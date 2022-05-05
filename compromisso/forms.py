061cfa72f1f777dad3d4ed2a718b75fed4ddec4a
Filipe Coutinho <fpcoutinho5@gmail.com>
2022-05-05 10:53:19 -0300

    edita_compromisso funcionando.

--- compromisso/forms.py
+++ compromisso/forms.py
@@ -2,7 +2,12 @@ from django import forms
 from . import models
 
 
-class CompromissoForm(forms.ModelForm):
+class CriaCompromissoForm(forms.ModelForm):
     class Meta:
         model = models.Compromisso
         fields = ['nome', 'local', 'data_inicial', 'data_final', 'obs']
+
+class EditaCompromissoForm(forms.ModelForm):
+    class Meta:
+        model = models.Compromisso
+        fields = ['nome', 'local', 'status', 'data_inicial', 'data_final', 'obs']
--- compromisso/views.py
+++ compromisso/views.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from .models import Compromisso
 from . import forms as compr_forms
 from django.contrib import messages
+from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 
 
@@ -17,40 +18,69 @@ def compromisso_list(request):
 @login_required(login_url="/accounts/login/")
 def compromisso_visualiza(request, comp_id):
     compromisso = Compromisso.objects.get(id=comp_id)
+    now = timezone.now()
+    if compromisso.data_final < now:
+        compromisso.status = 'Realizado'
     return render(request, 'compromisso/compromisso_visualiza.html', {'compromisso': compromisso})
+
+
+def checaData(request, compromissos):
+    # valida as datas do form
+    dataini = datetime.strptime(request.POST['data_inicial'], '%d/%m/%Y %H:%M:%S')
+    datafim = datetime.strptime(request.POST['data_final'], '%d/%m/%Y %H:%M:%S')
+    if dataini < datetime.now():
+        raise forms.ValidationError('Você está tentando criar um compromisso para uma data que já passou')
+    if dataini > datafim:
+        raise forms.ValidationError('A data final não pode ser anterior à data inicial')
+
+    # checa se o compromisso dá choque com outros compromissos do usuário
+    for compromisso in compromissos:
+        if (dataini <= compromisso.data_inicial < datafim) or (dataini < compromisso.data_final <= datafim):
+            raise forms.ValidationError(
+                'As datas do compromisso atual se chocam com a do compromisso ' + compromisso.nome)
+
+    return True
 
 
 @login_required(login_url="/accounts/login/")
 def compromisso_cria(request):
     if request.method == 'POST':
-        form = compr_forms.CompromissoForm(request.POST)
+        form = compr_forms.CriaCompromissoForm(request.POST)
         if form.is_valid():
             usuario = request.user
-
-            # valida as datas do form
-            dataini = datetime.strptime(request.POST['data_inicial'], '%d/%m/%Y %H:%M:%S')
-            datafim = datetime.strptime(request.POST['data_final'], '%d/%m/%Y %H:%M:%S')
-            if dataini < datetime.now():
-                raise forms.ValidationError('Você está tentando criar um compromisso para uma data que já passou')
-            if dataini > datafim:
-                raise forms.ValidationError('A data final não pode ser anterior à data inicial')
-
-            # checa se o compromisso dá choque com outros compromissos do usuário
             compromissos = Compromisso.objects.filter(autor=usuario)
-            for compromisso in compromissos:
-                if (dataini <= compromisso.data_inicial < datafim) or (dataini < compromisso.data_final <= datafim):
-                    raise forms.ValidationError(
-                        'As datas do compromisso atual se chocam com a do compromisso ' + compromisso.nome)
-
-            # salva o compromisso na lista
-            instance = form.save(commit=False)
-            instance.autor = usuario
-            instance.save()
-            return redirect('compromisso:list')
+            try:
+                if(checaData(request, compromissos)):
+                    instance = form.save(commit=False)
+                instance.autor = usuario
+                instance.save()
+                return redirect('compromisso:list')
+            except forms.ValidationError as e:
+                erro = messages.error(request, ''.join(e.messages))
+            return render(request, 'compromisso/compromisso_cria.html', {'form':form, 'erro':erro})
     else:
-        form = compr_forms.CompromissoForm()
+        form = compr_forms.CriaCompromissoForm()
     return render(request, 'compromisso/compromisso_cria.html', {'form': form})
-
+    
+    
+@login_required(login_url="/accounts/login/")
+def compromisso_edita(request, comp_id):
+    compromisso = Compromisso.objects.get(id=comp_id)
+    form = compr_forms.EditaCompromissoForm(request.POST or None, instance=compromisso)
+    if form.is_valid():
+        usuario = compromisso.autor
+        compromissos = Compromisso.objects.filter(autor=usuario)
+        compromissos2 = compromissos.exclude(id=comp_id)
+        try:
+            if(checaData(request, compromissos2)):
+                instance = form.save(commit=False)
+                instance.save()
+                return redirect('compromisso:list')
+        except forms.ValidationError as e:
+            erro = messages.error(request, ''.join(e.messages))
+            return render(request, 'compromisso/compromisso_edita.html', {'compromisso': compromisso,  'form':form, 'erro':erro})
+    return render(request, 'compromisso/compromisso_edita.html', {'compromisso': compromisso,  'form':form})
+    
 
 @login_required(login_url="/accounts/login/")
 def compromisso_deleta(request, comp_id):
@@ -58,12 +88,3 @@ def compromisso_deleta(request, comp_id):
     compromisso.delete()
     messages.success(request, "Compromisso excluído!")
     return redirect('compromisso:list')
-
-@login_required(login_url="/accounts/login/")
-def compromisso_edita(request, comp_id):
-    compromisso = Compromisso.objects.get(id=comp_id)
-    form = compr_forms.CompromissoForm(request.POST or None, instance=compromisso)
-    if form.is_valid():
-        form.save()
-        return redirect('compromisso:list')
-    return render(request, 'compromisso/compromisso_edita.html', {'compromisso': compromisso,  'form':form})
